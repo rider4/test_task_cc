1ef141d8fe5bd70a393d68c9a131d712c05cedcf
a.eugene <>
2024-07-27 00:48:45 +0200

    fix docker

--- devops/scripts/app-down.sh
+++ devops/scripts/app-down.sh
@@ -11,3 +11,11 @@ docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose
 
 echo -e "\n${GREEN}Down containers${RESET}"
 docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose.yml -p $NAME_PREFIX down -v
+
+echo -e "\n${GREEN}Setup DB${RESET}"
+$WINPTY docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose.yml -p $NAME_PREFIX  \
+  exec php sh -c "bin/console doctrine:database:drop --force"
+
+echo -e "\n${GREEN}Remove image${RESET}"
+docker rmi $NAME_PREFIX
+
--- devops/scripts/app-up.sh
+++ devops/scripts/app-up.sh
@@ -29,9 +29,8 @@ docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose
 echo -e "\n${GREEN}Removing containers${RESET}"
 docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose.yml -p $NAME_PREFIX rm -f
 
-## start application
 echo -e "\n${GREEN}Building images${RESET}"
-docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose.yml -p $NAME_PREFIX build --pull
+docker buildx build --tag $NAME_PREFIX ${DEVOPS_DIR}/image/php
 
 echo -e "\n${GREEN}Up containers${RESET}"
 docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose.yml -p $NAME_PREFIX up -d --force-recreate
@@ -39,5 +38,15 @@ docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose
 echo -e "\n${GREEN}Installing dependencies${RESET}"
 $WINPTY docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose.yml -p $NAME_PREFIX  \
   exec php sh -c "composer install -a -n --prefer-dist --ignore-platform-reqs;"
+
+echo -e "\n${GREEN}Installing dependencies${RESET}"
 $WINPTY docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose.yml -p $NAME_PREFIX  \
-  exec php sh -c "composer app:up-db;"
+  exec php sh -c "composer cache:preparing"
+
+echo -e "\n${GREEN}Setup DB${RESET}"
+$WINPTY docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose.yml -p $NAME_PREFIX  \
+  exec php sh -c "bin/console doctrine:database:create"
+
+echo -e "\n${GREEN}Migration apply${RESET}"
+$WINPTY docker-compose --project-directory ${DEVOPS_DIR} -f ${DEVOPS_DIR}/docker-compose.yml -p $NAME_PREFIX  \
+  exec php sh -c "bin/console doctrine:migrations:migrate -n"
